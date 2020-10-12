bc1d3f6055240d048aeeb151bf953649bfe26db8
Fahran Kamili <fkamili@thezebra.com>
2020-10-11 20:32:33 -0500

    Create invalid rows cleaner

---
+++ stock_price_cleaner_remove_invalid_rows.py
@@ -0,0 +1,12 @@
+import pandas
+from pandas import DataFrame
+
+FILENAME = 'stock_price_small.csv'
+stock_price_df = pandas.read_csv(FILENAME)  # type: DataFrame
+
+stock_price_df.drop(
+    stock_price_df[stock_price_df['open'] == 'open'].index,
+    inplace=True
+)
+
+stock_price_df.to_csv('stock_price_small.csv')
