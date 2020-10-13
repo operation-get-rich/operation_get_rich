60445aaf18e55a5f16482a0076bc90173864a235
Fahran Kamili <fkamili@thezebra.com>
2020-10-12 23:58:29 -0500

    Merge Randy's changes into gap_up_stock_segmenters.py

--- gap_up_stock_segmenters.py
+++ gap_up_stock_segmenters.py
@@ -1,11 +1,12 @@
+import os
 import pandas
 from pandas import DataFrame
 
-from utils import create_dir, get_date, get_date_time, get_date_string
+from utils import get_date_time, get_date, create_dir, get_date_string
 
 GAP_UP_THRESHOLD = 0.15
 VOLUME_THRESHOLD = 1e+06
-RAW_STOCK_PRICE_SOURCE_FILENAME = 'stock_price_small.csv'
+RAW_STOCK_PRICE_DIR = 'raw_data'
 GAPED_UP_STOCKS_DIR_NAME = 'gaped_up_stocks'
 
 
@@ -13,19 +14,12 @@ def is_gapped_up(open_price, close_price):
     return (open_price - close_price) / close_price > GAP_UP_THRESHOLD
 
 
-def find_segments():
+raw_stock_list = os.listdir(RAW_STOCK_PRICE_DIR)
+for stock_file in raw_stock_list:
+    stock_path = os.path.join(RAW_STOCK_PRICE_DIR, stock_file)
+
     print("Loading Data:", flush=True)
-
-    stock_price_df = pandas.read_csv(RAW_STOCK_PRICE_SOURCE_FILENAME)  # type: DataFrame
-    stock_price_df['open'] = pandas.to_numeric(stock_price_df['open'])
-    stock_price_df['close'] = pandas.to_numeric(stock_price_df['close'])
-    stock_price_df['low'] = pandas.to_numeric(stock_price_df['low'])
-    stock_price_df['high'] = pandas.to_numeric(stock_price_df['high'])
-    stock_price_df['volume'] = pandas.to_numeric(stock_price_df['volume'])
-
-    print("Finding Segments:", flush=True)
-
-    segments = []  # [('AAPL', '01/17/20'), ('AAPL', '01/18/20')]
+    stock_price_df = pandas.read_csv(stock_path)  # type: DataFrame
 
     current_ticker = None
     previous_ticker = None
@@ -33,17 +27,19 @@ def find_segments():
     close_price = None
     cummulative_volume = 0
 
+    segments = []  # [('AAPL', '01/17/20'), ('AAPL', '01/18/20')]
+    print("Starting Segmentation:", flush=True)
     for index in reversed(range(len(stock_price_df.index))):
         row = stock_price_df.loc[index]
+
         # Reset everything when new ticker arrives
         if current_ticker is None or row.ticker != previous_ticker:
             current_ticker = row.ticker
             open_price = None
             close_price = None
             cummulative_volume = 0
-
-        print("Current Ticker: ", current_ticker, flush=True)
-        print("Current Index: ", index, flush=True)
+            print("Current Ticker: ", current_ticker, flush=True)
+            print("Current Index: ", index, flush=True)
 
         if get_date_time(row.time).hour < 10 and open_price is None:
             open_price = row.open
@@ -61,13 +57,12 @@ def find_segments():
                 open_price = None
                 close_price = None
                 cummulative_volume = 0
+
         previous_ticker = current_ticker
-    return segments
 
-
-def save_segments(segments):
-    create_dir(GAPED_UP_STOCKS_DIR_NAME)
     stock_price_df['just_date'] = stock_price_df.apply(lambda row: get_date(row.time), axis=1)
+
+    create_dir('./%s' % GAPED_UP_STOCKS_DIR_NAME)
     for ticker_segment, date_segment in segments:
         print("Writing Ticker: ", ticker_segment, flush=True)
         get_date_string(date_segment)
@@ -81,7 +76,3 @@ def save_segments(segments):
         the_dir = create_dir('./{}/{}'.format(GAPED_UP_STOCKS_DIR_NAME, ticker_segment))
 
         the_segment.to_csv(path_or_buf='{}/{}_{}'.format(the_dir, ticker_segment, date_segment), index=False)
-
-
-segments = find_segments()
-save_segments(segments)
