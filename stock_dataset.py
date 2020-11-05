3b7f94104148ebbbcd9ad0f044866a0547ad5805
Fahran Kamili <fkamili@thezebra.com>
2020-11-05 13:59:33 -0800

    Group normalization code into PercentChangeNormalizer

--- stock_dataset.py
+++ stock_dataset.py
@@ -26,6 +26,59 @@ VWAP_COLUMN_INDEX = 5
 EMA_COLUMN_INDEX = 6
 
 
+class PercentChangeNormalizer:
+    @classmethod
+    def normalize_volume(cls, data):
+        data = np.copy(data)
+        for batch_index in range(len(data)):
+            current_batch_data = data[batch_index]
+            current_batch_data[:, VOLUME_COLUMN_INDEX] -= VOLUME_MEAN
+            current_batch_data[:, VOLUME_COLUMN_INDEX] /= VOLUME_STD
+        return torch.Tensor(data)
+
+    @classmethod
+    def normalize_price_into_percent_change(cls, data):
+        data = np.copy(data)
+        for batch_index in range(len(data)):
+            current_batch_data = data[batch_index]
+            anchor_open_price = current_batch_data[0, OPEN_COLUMN_INDEX]
+            anchor_close_price = current_batch_data[0, CLOSE_COLUMN_INDEX]
+            anchor_low_price = current_batch_data[0, LOW_COLUMN_INDEX]
+            anchor_high_price = current_batch_data[0, HIGH_COLUMN_INDEX]
+            anchor_vwap = current_batch_data[0, VWAP_COLUMN_INDEX]
+            anchor_ema = current_batch_data[0, EMA_COLUMN_INDEX]
+
+            for i in range(0, current_batch_data.shape[0]):
+                current_batch_data[i, OPEN_COLUMN_INDEX] = cls._compute_percent_change(
+                    anchor_open_price,
+                    current_batch_data[i, OPEN_COLUMN_INDEX])
+
+                current_batch_data[i, CLOSE_COLUMN_INDEX] = cls._compute_percent_change(
+                    anchor_close_price,
+                    current_batch_data[i, CLOSE_COLUMN_INDEX])
+
+                current_batch_data[i, LOW_COLUMN_INDEX] = cls._compute_percent_change(
+                    anchor_low_price,
+                    current_batch_data[i, LOW_COLUMN_INDEX])
+
+                current_batch_data[i, HIGH_COLUMN_INDEX] = cls._compute_percent_change(
+                    anchor_high_price,
+                    current_batch_data[i, HIGH_COLUMN_INDEX])
+
+                current_batch_data[i, VWAP_COLUMN_INDEX] = cls._compute_percent_change(
+                    anchor_vwap,
+                    current_batch_data[i, VWAP_COLUMN_INDEX])
+
+                current_batch_data[i, EMA_COLUMN_INDEX] = cls._compute_percent_change(
+                    anchor_ema,
+                    current_batch_data[i, EMA_COLUMN_INDEX])
+        return torch.Tensor(data)
+
+    @classmethod
+    def _compute_percent_change(cls, p1, p2):
+        return (p2 / p1) - 1
+
+
 class StockDataset(torch.utils.data.Dataset):
     """
     Responsible to return batches of `gaped_up_stocks_early_volume_1e5_gap_10` stock data segments.
@@ -67,9 +120,6 @@ class StockDataset(torch.utils.data.Dataset):
 
         selected_segment_np = selected_segment_df[TECHNICAL_INDICATOR_PERIOD:].to_numpy()
 
-        self._normalize_price_data_using_percent_change(selected_segment_np)
-        self._normalize_volume(selected_segment_np)
-
         selected_segment_length = selected_segment_np.shape[0]
         if selected_segment_length < SEQUENCE_LENGTH:
             selected_segment_np = np.vstack(
@@ -84,46 +134,6 @@ class StockDataset(torch.utils.data.Dataset):
             selected_segment_np = selected_segment_np[start: start + SEQUENCE_LENGTH, :]
 
             return selected_segment_np, SEQUENCE_LENGTH
-
-    def _normalize_volume(self, selected_segment_np):
-        selected_segment_np[:, VOLUME_COLUMN_INDEX] -= VOLUME_MEAN
-        selected_segment_np[:, VOLUME_COLUMN_INDEX] /= VOLUME_STD
-
-    def _normalize_price_data_using_percent_change(self, selected_segment_np):
-        anchor_open_price = selected_segment_np[0, OPEN_COLUMN_INDEX]
-        anchor_close_price = selected_segment_np[0, CLOSE_COLUMN_INDEX]
-        anchor_low_price = selected_segment_np[0, LOW_COLUMN_INDEX]
-        anchor_high_price = selected_segment_np[0, HIGH_COLUMN_INDEX]
-        anchor_vwap = selected_segment_np[0, VWAP_COLUMN_INDEX]
-        anchor_ema = selected_segment_np[0, EMA_COLUMN_INDEX]
-
-        for i in range(0, selected_segment_np.shape[0]):
-            selected_segment_np[i, OPEN_COLUMN_INDEX] = self._compute_percent_change(
-                anchor_open_price,
-                selected_segment_np[i, OPEN_COLUMN_INDEX])
-
-            selected_segment_np[i, CLOSE_COLUMN_INDEX] = self._compute_percent_change(
-                anchor_close_price,
-                selected_segment_np[i, CLOSE_COLUMN_INDEX])
-
-            selected_segment_np[i, LOW_COLUMN_INDEX] = self._compute_percent_change(
-                anchor_low_price,
-                selected_segment_np[i, LOW_COLUMN_INDEX])
-
-            selected_segment_np[i, HIGH_COLUMN_INDEX] = self._compute_percent_change(
-                anchor_high_price,
-                selected_segment_np[i, HIGH_COLUMN_INDEX])
-
-            selected_segment_np[i, VWAP_COLUMN_INDEX] = self._compute_percent_change(
-                anchor_vwap,
-                selected_segment_np[i, VWAP_COLUMN_INDEX])
-
-            selected_segment_np[i, EMA_COLUMN_INDEX] = self._compute_percent_change(
-                anchor_ema,
-                selected_segment_np[i, EMA_COLUMN_INDEX])
-
-    def _compute_percent_change(self, p1, p2):
-        return (p2 / p1) - 1
 
     def __len__(self):
         return len(self.segment_list)
