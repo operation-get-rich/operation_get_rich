9f8b9b3676eaa61c38f7690e5d038ffd9f91ad4d
Fahran Kamili <fkamili@thezebra.com>
2020-11-05 13:59:03 -0800

    Fix ProfitReward computation

--- objectives.py
+++ objectives.py
@@ -1,26 +1,25 @@
-
-
 def ProfitReward(trade_sequence, market_sequence):
     time_length = trade_sequence.shape[0]
-    capital = 10000
+    capital = 1
     shares = 0
     for t in range(time_length):
         current_trade = trade_sequence[t]
         current_price = market_sequence[t]
-        if current_trade > 0: # buying
-            shares += current_trade * capital / current_price
-            capital -= current_trade * capital
-        else: # selling
-            capital -= shares * current_price * current_trade
-            shares += shares * current_trade
+        if current_trade > 0:  # buying
+            bought_shares = (current_trade * capital) / current_price
+            shares = shares + bought_shares
+            capital = capital - (bought_shares * current_price)
+        elif current_trade < 0:  # selling
+            sold_shares = shares * current_trade  # sold_shares < 0
+            shares = shares + sold_shares
+            capital = capital - (sold_shares * current_price)
+
         # print("Current Price: ", current_price)
         # print("Current Trade: ", current_trade)
         # print("Current Capital: ", capital)
         # print("Current Shares: ", shares)
 
-
     final_price = market_sequence[-1]
-    if shares > 0:
-        capital += shares * final_price
-    
-    return capital - 10000
+    capital = capital + (shares * final_price)
+
+    return capital - 1
