0548e0f24cac99fa2aa62257d6c0f1d08e1317e3
Fahran Kamili <fkamili@thezebra.com>
2020-12-08 07:15:11 -0600

    Fix Polygon tick by tick downloader

--- downloaders/polygon_tick_by_tick_downloader.py
+++ downloaders/polygon_tick_by_tick_downloader.py
@@ -1,10 +1,12 @@
 import csv
+import json
 import os
 
 import alpaca_trade_api as tradeapi
 
 from config import ALPACA_KEY_ID, ALPACA_SECRET_KEY, ALPACA_BASE_URL
-from utils import create_dir, convert_pandas_timestamp_to_formatted_string, retry_download
+from utils import create_dir, convert_pandas_timestamp_to_formatted_string, retry_download, get_current_directory, \
+    update_state
 
 api = tradeapi.REST(
     key_id=ALPACA_KEY_ID,
@@ -12,15 +14,18 @@ api = tradeapi.REST(
     base_url=ALPACA_BASE_URL,
 )
 
-raw_gaped_up_dir = '../datas/alpaca_gaped_up_stocks_early_volume_1e5_gap_10_by_date'
+STATE_FILE_LOCATION = f'{get_current_directory(__file__)}/polygon_tick_by_tick_downloader_state.json'
+raw_gaped_up_dir = '../datas/polygon_early_day_gap_segmenter_parallel'
 
-save_root_dir = '../datas/alpaca_gapped_up_tick_by_tick'
+save_root_dir = '../datas/polygon_gapped_up_tick_by_tick'
 create_dir(save_root_dir)
 
 
 def _download_quotes(symbol, the_date):
     quotes = []
     lowerbound_timestamp = None
+    lowerbound_timestamp_obj = None  # For logging only
+    lowerbound_timestamps = []
     quotes.append([
         'sip_timestamp',
         'bid_price',
@@ -31,6 +36,10 @@ def _download_quotes(symbol, the_date):
         'ask_exchange'
     ])
     while True:
+        if lowerbound_timestamp in lowerbound_timestamps:
+            # market close early, lowerbound_timestamp repeats indefinitely
+            return quotes
+        print(f'Downloading {symbol}, {the_date}, lowerbound_timestamp={lowerbound_timestamp_obj}')
         symbol_quotes = _history_quote_v2_wrapper(lowerbound_timestamp, symbol, the_date)
 
         for quote in symbol_quotes:
@@ -45,7 +54,9 @@ def _download_quotes(symbol, the_date):
             ])
 
         if symbol_quotes[-1].sip_timestamp.hour < 16:
+            lowerbound_timestamps.append(lowerbound_timestamp)
             lowerbound_timestamp = symbol_quotes[-1]._raw['sip_timestamp']
+            lowerbound_timestamp_obj = symbol_quotes[-1].sip_timestamp  # for logging only
         else:
             break
     return quotes
@@ -61,11 +72,25 @@ def _history_quote_v2_wrapper(lowerbound_timestamp, symbol, the_date):
     return symbol_quotes
 
 
+def update_finished_file_state(state, the_date, symbol):
+    if not 'finished' in state:
+        state['finished'] = []
+    state['finished'].append(f'{the_date}/{symbol}')
+
+
 def main():
+    with open('./polygon_tick_by_tick_downloader_state.json') as state_file:
+        state = json.load(state_file)
+
+    finished_segments = set(state['finished']) if 'finished' in state else {}
+
     for date_dir in sorted(os.listdir('%s' % raw_gaped_up_dir)):
         for stock_filename in sorted(os.listdir(f'{raw_gaped_up_dir}/{date_dir}')):
-            symbol = stock_filename[0:-26]
+            symbol = stock_filename[0:-15]
             the_date = date_dir
+
+            if f'{the_date}/{symbol}' in finished_segments:
+                continue
 
             print(f'Downloading {symbol} @ {the_date}')
 
@@ -78,6 +103,7 @@ def main():
                 writer = csv.writer(f)
                 for quote in quotes:
                     writer.writerow(quote)
+            update_state(STATE_FILE_LOCATION, update_finished_file_state, the_date, symbol)
             print(f'Finished Writing into {stock_tick_by_tick_csv}')
 
 
