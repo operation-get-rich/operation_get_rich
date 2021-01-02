aad855df15499d7a2e99526a3e9291040a7dce4b
Fahran Kamili <fkamili@thezebra.com>
2021-01-01 19:11:00 -0600

    Update code that computes dataset statitsitcs

---
+++ stats_calculator_gaped_up_stocks_early_volume.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+import pandas as pd
+
+DATASET_NAME = 'gaped_up_stocks_early_volume_1e5_gap_10'
+ROOT_DATA_DIR = './%s' % DATASET_NAME
+
+entire_segments_df = pd.DataFrame()
+
+for company in os.listdir(ROOT_DATA_DIR):
+    for segment in os.listdir(os.path.join(ROOT_DATA_DIR, company)):
+        segment_file_path = os.path.join(ROOT_DATA_DIR, company, segment)
+        entire_segments_df = entire_segments_df.append(pd.read_csv(segment_file_path))
+
+entire_segments_descriptive_stats = entire_segments_df.describe()
+
+stats_dict = {}
+for key in entire_segments_descriptive_stats.keys():
+    if key not in stats_dict:
+        stats_dict[key] = {}
+    for inner_key in entire_segments_descriptive_stats[key].keys():
+        stats_dict[key][inner_key] = entire_segments_descriptive_stats[key][inner_key]
+
+try:
+    with open('./statistics_dataset.json', 'r') as f:
+        statistics_dataset = json.load(f)
+except FileNotFoundError as e:
+    statistics_dataset = {}
+
+statistics_dataset[DATASET_NAME] = stats_dict
+
+with open('./statistics_dataset.json', 'w') as f:
+    json.dump(statistics_dataset, f)
+
