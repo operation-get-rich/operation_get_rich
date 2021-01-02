98472405778bc31b7ce29ce06d5ad21862d77aef
Fahran Kamili <fkamili@thezebra.com>
2021-01-01 19:11:15 -0600

    Add random utils

---
+++ experiment_sniper_gru/sniper_segmenter_analyzer.py
@@ -0,0 +1,15 @@
+import os
+
+from directories import DATA_DIR
+
+root_dir = f'{DATA_DIR}/sniper_training_data_1'
+zero_count = 0
+one_count = 0
+for file in os.listdir(root_dir):
+    if '0' in file.split('.')[0][-2:]:
+        zero_count += 1
+    else:
+        one_count +=1
+
+print(f'Zero Count {zero_count}')
+print(f'One Count {one_count}')
