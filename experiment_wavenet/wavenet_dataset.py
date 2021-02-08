83b6caabb3866c6ff2ba09388a87559f8fe82c65
Fahran Kamili <fkamili@thezebra.com>
2021-02-07 18:04:30 -0600

    Create backbone for WavenetDataset

---
+++ experiment_wavenet/wavenet_dataset.py
@@ -0,0 +1,12 @@
+import torch
+
+
+class WavenetDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        pass
