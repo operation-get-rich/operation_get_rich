b62ffdaffa4a4fa9b53e86e0ddc6aaf68672cce0
Fahran Kamili <fkamili@thezebra.com>
2021-02-12 17:40:39 -0600

    Implement the wavenet model

--- experiment_wavenet/wavenet_model.py
+++ experiment_wavenet/wavenet_model.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.autograd.grad_mode import F
 
 
 class WaveNetModel(nn.Module):
@@ -82,3 +83,58 @@ class WaveNetModel(nn.Module):
                                     out_channels=1,
                                     kernel_size=1,
                                     bias=True)
+
+    def wavenet(self, input):
+        x = self.start_conv(input)
+
+        skip = 0
+
+        # WaveNet layers
+        for i in range(self.blocks * self.layers):
+            #            |----------------------------------------|     *residual*
+            #            |                                        |
+            #            |    |-- conv -- tanh --|                |
+            # -> dilate -|----|                  * ----|-- 1x1 -- + -->	*input*
+            #                 |-- conv -- sigm --|     |
+            #                                         1x1
+            #                                          |
+            # ---------------------------------------> + ------------->	*skip*
+            (dilation, init_dilation) = self.dilations[i]
+
+            residual = dilate(x, dilation, init_dilation)
+
+            # dilated convolution
+            filter = self.filter_convs[i](residual)
+            filter = F.tanh(filter)
+            gate = self.gate_convs[i](residual)
+            gate = F.sigmoid(gate)
+            x = filter * gate
+
+            # parametrized skip connection
+            s = x
+            if x.size(2) != 1:
+                s = dilate(x, 1, init_dilation=dilation)
+            s = self.skip_convs[i](s)
+            try:
+                skip = skip[:, :, -s.size(2):]  # to make element-wise addition makes sense
+            except:
+                skip = 0
+            skip = s + skip
+
+            x = self.residual_convs[i](x)
+            x = x + residual[:, :, (self.kernel_size - 1):]
+
+        x = F.relu(skip)
+        x = F.relu(self.end_conv_1(x))
+        x = self.end_conv_2(x)
+
+        return x
+
+    def forward(self, input):
+        x = self.wavenet(input)
+        return x
+
+
+def dilate(x, dilation, init_dilation=1, pad_start=True):
+    # TODO: Implement this
+    NotImplemented
